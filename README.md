672c375e39c3a58df126d52f723a0a57433e68dc
rhn014 <Roland.Hyden@RegionHalland.se>
2019-05-15 14:44:53 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# region-halland-page-menu-text
-Text för navigersingtext i en meny
+# Skapa en textruta med plats för menu text på en sida
+
+## Hur man använder Region Hallands plugin "region-halland-acf-page-menu-text"
+
+Nedan följer instruktioner hur du kan använda pluginet "region-halland-acf-page-menu-text".
+
+
+## Användningsområde
+
+Denna plugin skapar en textruta med plats för menu text på en sida
+
+
+## Installation och aktivering
+
+```sh
+A) Hämta pluginen via Git eller läs in det med Composer
+B) Installera Region Hallands plugin i Wordpress plugin folder
+C) Aktivera pluginet inifrån Wordpress admin
+```
+
+
+## Hämta hem pluginet via Git
+
+```sh
+git clone https://github.com/RegionHalland/region-halland-acf-page-menu-text.git
+```
+
+
+## Läs in pluginen via composer
+
+Dessa två delar behöver du lägga in i din composer-fil
+
+Repositories = var pluginen är lagrad, i detta fall på github
+
+```sh
+"repositories": [
+  {
+    "type": "vcs",
+    "url": "https://github.com/RegionHalland/region-halland-acf-page-menu-text.git"
+  },
+],
+```
+Require = anger vilken version av pluginen du vill använda, i detta fall version 1.0.0
+
+OBS! Justera så att du hämtar aktuell version.
+
+```sh
+"require": {
+  "regionhalland/region-halland-acf-page-menu-text": "1.0.0"
+},
+```
+
+
+## Visa navigation-texten på en sida via "Blade"
+
+OBS! Notera att man i en meny även måste ange ID för den post man vill visa 
+
+```sh
+<div>
+  {{ get_region_halland_acf_page_menu_text(28) }}
+</div>
+```
+
+## Versionhistorik
+
+### 1.0.0
+- Första version
---
+++ region_halland_acf_page_menu_text.php
@@ -0,0 +1,91 @@
+<?php
+
+	/**
+	 * @package Region Halland ACF Page Menu Text
+	 */
+	/*
+	Plugin Name: Region Halland ACF Page Menu Text
+	Description: ACF-fält för menu text
+	Version: 1.0.0
+	Author: Roland Hydén
+	License: MIT
+	Text Domain: regionhalland
+	*/
+
+	// Anropa function om ACF är installerad
+	add_action('acf/init', 'my_acf_add_page_menu_text_field_groups');
+
+	// Function för att lägga till "field groups"
+	function my_acf_add_page_menu_text_field_groups() {
+
+		if (function_exists('acf_add_local_field_group')):
+
+			acf_add_local_field_group(array(
+			    'key' => 'group_1000143',
+			    'title' => 'Navigering',
+			    'fields' => array(
+			        0 => array(
+			        	'key' => 'field_1000144',
+			            'label' => __('Navigation', 'regionhalland'),
+			            'name' => 'name_1000145',
+			            'type' => 'text',
+			            'instructions' => __('Skriv din meny-text här', 'regionhalland'),
+			            'required' => 0,
+			            'conditional_logic' => 0,
+			            'wrapper' => array(
+			                'width' => '',
+			                'class' => '',
+			                'id' => '',
+			            ),
+			            'default_value' => '',
+			            'placeholder' => '',
+			            'prepend' => '',
+			            'append' => '',
+			            'maxlength' => '',
+			        ),
+			    ),
+			    'location' => array(
+			        0 => array(
+			            0 => array(
+			                'param' => 'post_type',
+			                'operator' => '==',
+			                'value' => 'page',
+			            ),
+			        )
+			    ),
+			    'menu_order' => 0,
+			    'position' => 'normal',
+			    'style' => 'default',
+			    'label_placement' => 'top',
+			    'instruction_placement' => 'label',
+			    'hide_on_screen' => '',
+			    'active' => 1,
+			    'description' => '',
+			));
+
+		endif;
+	
+	}
+
+	// Returnera navigation text
+	function get_region_halland_acf_page_menu_text($id) {
+		return get_field('name_1000145', $id);	
+	}
+	
+	// Metod som anropas när pluginen aktiveras
+	function region_halland_acf_page_menu_text_activate() {
+		// Ingenting just nu...
+	}
+
+	// Metod som anropas när pluginen avaktiveras
+	function region_halland_acf_page_menu_text_deactivate() {
+		// Ingenting just nu...
+	}
+	
+	// Vilken metod som ska anropas när pluginen aktiveras
+	register_activation_hook( __FILE__, 'region_halland_acf_page_menu_text_activate');
+
+	// Vilken metod som ska anropas när pluginen avaktiveras
+	register_deactivation_hook( __FILE__, 'region_halland_acf_page_menu_text_deactivate');
+
+?>
